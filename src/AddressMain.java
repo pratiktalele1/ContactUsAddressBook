b2a0810c8ff7fdcf21f39c9c000e813fc96cd7de
pratiktalele1 <pratiktalele1@gmail.com>
2021-08-08 13:06:22 +0530

    commiting add person detail to new contact book

--- src/AddressMain.java
+++ src/AddressMain.java
@@ -5,10 +5,13 @@ public class AddressMain {
 		System.out.println("Welcome to Address Book");
 		Contact contact = new Contact();
 		UC1 uc1 = new UC1();
-		uc1.add(contact.map,contact.names);
-		uc1.print(contact.map,contact.names);
-		uc1.add(contact.map,contact.names);
-		uc1.print(contact.map,contact.names);
-		
+		uc1.add(contact.map, contact.names);
+		uc1.print(contact.map, contact.names);
+
+		Contact2 contact2 = new Contact2();
+		UC2 uc2 = new UC2();
+		uc2.add(contact2.map, contact2.names);
+		uc2.print(contact2.map, contact2.names);
+
 	}
 }
