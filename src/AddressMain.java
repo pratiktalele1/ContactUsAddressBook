80d62c33ece6420d3667f74bec3d0fe435d96059
pratiktalele1 <pratiktalele1@gmail.com>
2021-08-08 12:55:30 +0530

    commiting add person details

--- src/AddressMain.java
+++ src/AddressMain.java
@@ -1,10 +1,14 @@
 
-
 public class AddressMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
-
+		Contact contact = new Contact();
+		UC1 uc1 = new UC1();
+		uc1.add(contact.map,contact.names);
+		uc1.print(contact.map,contact.names);
+		uc1.add(contact.map,contact.names);
+		uc1.print(contact.map,contact.names);
 		
 	}
 }
